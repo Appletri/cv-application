c4d2394f93e2cae4708efe67248797ac4c9c14b2
appletri <tri_duc_lam@hotmail.com>
2022-06-13 13:08:03 -0600

    made app into a hook

--- src/App.js
+++ src/App.js
@@ -1,68 +1,64 @@
 import './styles/App.css';
-import { Component } from 'react';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import GeneralInfo from './components/GeneralInfo';
 import EducExp from './components/EducExp';
 import WorkExp from './components/WorkExp';
 import Skills from './components/Skills';
 import arrow from './assets/Arrow-To-icon.png';
 
-class App extends Component {
-  constructor() {
-    super();
-    this.toggleMoreFeatures = this.toggleMoreFeatures.bind(this);
-    this.state = {
-      editState:{ state: 'false' }  
-    }
-  }
-  
-  toggleMoreFeatures(e) {
-    let editState = this.state.editState;
-    if (editState.state === 'false') {
-      editState.state = 'true';
+function App() {
+  const [editState, setEditState] = useState('false')
+
+  const toggleMoreFeatures = () => {
+    let state = editState;
+    if (state === 'false') {
+      state = 'true';
     } else {
-      editState.state = 'false';
+      state = 'false';
     }
     
-    this.setState(editState);
+    setEditState(state);
   }
-  
-  render() {
-    const addButts = document.querySelectorAll('.add');
-    const removeButts = document.querySelectorAll('.remove');
-    const editState = this.state.editState;
 
-    if (editState.state === 'false') {
+  useEffect (() => {
+    const addButts = document.querySelectorAll('.add');
+    const removeButts = document.querySelectorAll('.remove');   
+    
+    if (editState === 'false') {
       addButts.forEach((button) => button.className = 'add hidden');
       removeButts.forEach((button) => button.className = 'remove hidden');
     } else {
       addButts.forEach((button) => button.className = 'add')
       removeButts.forEach((button) => button.className = 'remove')
     }
-    return (
-      <div className='app'>
-        <div className='edit-wrapper'>
-          <button className='edit-button' onClick={this.toggleMoreFeatures}>
-            <div className='square-icon' />
-          </button>
-          <div className='tutorial'>
-            <img className='arrow' src={arrow} alt='arrow'></img> Click this button to reveal more edit features
-          </div>
-        </div>
-        <div className='cv'>
-          <GeneralInfo />
-          <div className='info'>
-            <div className='left-side'>
-              <EducExp />
-              <Skills />
-            </div>
-            <hr></hr>
-            <WorkExp />
-          </div>
-        </div>
+  }, [editState]);
+
+
+
+ return (
+  <div className='app'>
+    <div className='edit-wrapper'>
+      <button className='edit-button' onClick={toggleMoreFeatures}>
+        <div className='square-icon' />
+      </button>
+      <div className='tutorial'>
+        <img className='arrow' src={arrow} alt='arrow'></img> Click this button to reveal more edit features
       </div>
-    );
-  }
+    </div>
+    <div className='cv'>
+      <GeneralInfo />
+      <div className='info'>
+        <div className='left-side'>
+          <EducExp />
+          <Skills />
+        </div>
+        <hr></hr>
+        <WorkExp />
+      </div>
+    </div>
+  </div>
+  );
+  
 }
 
 export default App;
